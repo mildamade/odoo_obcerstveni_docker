3389f71201f35e3f0ba167b198cc36d06c7a91fa
mildamade <milda.dvorak@gmail.com>
2018-07-23 00:15:28 +0200

    oprava backup

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@ export PGPASSWORD="odoo"
 docker exec -i odoodvoraktatadocker_odoo11_1 /bin/bash -c \
 "export PGPASSWORD="odoo" \
 && /usr/bin/pg_dump --host=53e6d59fa2fb --dbname=DVORAK --username=odoo" \
-> ./dvorak_postgresql.dump
+> /data/odoo_backup_db/dvorak_postgresql.dump
